ebddaebd06b2ff916ed0d8a1aa1e18ac419e211c
sonallux <sonallux@gmx.de>
2023-08-15 21:49:18 +0200

    Add albums and related artists to artist page

--- src/app/pages/artist/artist.component.ts
+++ src/app/pages/artist/artist.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { map, Observable, shareReplay, switchMap } from 'rxjs';
 import { HeroData } from '../../shared/hero-header/hero-header.component';
-import { Artist, Track } from '@spotify/web-api-ts-sdk';
+import { Album, Artist, Page, SimplifiedAlbum, Track } from '@spotify/web-api-ts-sdk';
 import { Breakpoint, TailwindBreakpointObserver } from '../../shared/services/tailwind-breakpoint-observer.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { SpotifyArtistApi } from '../../spotify-client/api/artist-api.service';
 import { ActivatedRoute } from '@angular/router';
+import { CardItem } from '../../shared/clickable-card/clickable-card.component';
 
 @Component({
   selector: 'app-artist',
@@ -20,6 +21,10 @@ export class ArtistComponent {
   artistHeroData$: Observable<HeroData>;
 
   topTracks$: Observable<Track[]>;
+
+  albums$: Observable<CardItem[]>;
+
+  relatedArtists$: Observable<CardItem[]>
 
   constructor(private artistApi: SpotifyArtistApi,
               private breakpointObserver: TailwindBreakpointObserver,
@@ -46,8 +51,18 @@ export class ArtistComponent {
 
     this.topTracks$ = artistId$.pipe(
       switchMap(artistId => this.artistApi.getArtistsTopTracks(artistId, {market: 'DE'})),
-      map(result => result.tracks)
-    )
+      map(result => result.tracks.slice(0, 5))
+    );
+
+    this.albums$ = artistId$.pipe(
+      switchMap(artistId => this.artistApi.getArtistsAlbums(artistId, {market: 'DE'})),
+      map(page => page.items.slice(0, 5).map(mapAlbumToCardItem))
+    );
+
+    this.relatedArtists$ = artistId$.pipe(
+      switchMap(artistId => this.artistApi.getArtistsRelatedArtists(artistId)),
+      map(artists => artists.artists.slice(0, 5).map(mapArtistToCardItem))
+    );
   }
 }
 
@@ -57,4 +72,22 @@ function mapArtistToHeroData(artist: Artist) {
     title: artist.name,
     imageUrl: artist.images[0].url ?? ''
   };
+}
+
+function mapAlbumToCardItem(album: SimplifiedAlbum): CardItem {
+  return {
+    title: album.name,
+    imageUrl: album.images[0].url,
+    subtitle: `${album.release_date} ${album.album_type}`,
+    link: `/album/${album.id}`
+  }
+}
+
+function mapArtistToCardItem(artist: Artist): CardItem {
+  return {
+    title: artist.name,
+    imageUrl: artist.images[0].url,
+    subtitle: 'Artist',
+    link: `/artist/${artist.id}`
+  }
 }
