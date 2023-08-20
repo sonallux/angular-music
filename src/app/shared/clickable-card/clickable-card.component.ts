26c11b51e4e4a17c6eb6ec99bcf127491a239708
sonallux <sonallux@gmx.de>
2023-08-20 17:27:33 +0200

    Add custom theme for angular material

--- src/app/shared/clickable-card/clickable-card.component.ts
+++ src/app/shared/clickable-card/clickable-card.component.ts
@@ -2,8 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-clickable-card',
-  templateUrl: './clickable-card.component.html',
-  styleUrls: ['./clickable-card.component.scss']
+  templateUrl: './clickable-card.component.html'
 })
 export class ClickableCardComponent {
   @Input({required: true}) item!: CardItem | null
