2573896edc1bc7c8329dcbe149e43d0906039bca
sonallux <sonallux@gmx.de>
2023-10-01 10:47:17 +0200

    feat: add user-flows

---
+++ user-flows/flows/open-featured-playlist.uf.ts
@@ -0,0 +1,26 @@
+import { UserFlowProvider, } from '@push-based/user-flow';
+import { createSpotifyTokenCookie } from '../support';
+
+const userFlowProvider: UserFlowProvider = {
+  flowOptions: { name: 'Open featured playlist' },
+  interactions: async ({ page, flow, collectOptions }) => {
+    await page.setCookie(await createSpotifyTokenCookie())
+
+    await flow.navigate(collectOptions.url, {stepName: 'Navigate to Home'});
+
+    await page.waitForSelector('h1');
+
+    await flow.startTimespan({ stepName: 'Select first featured playlist' });
+
+    await page.click('app-clickable-card:first-of-type');
+    await page.waitForSelector('main app-hero-header h1');
+
+    await flow.endTimespan();
+
+    await flow.snapshot({ stepName: 'Playlist page' });
+  }
+};
+
+module.exports = userFlowProvider;
+
+
---
+++ user-flows/flows/open-new-release.uf.ts
@@ -0,0 +1,26 @@
+import { UserFlowProvider, } from '@push-based/user-flow';
+import { createSpotifyTokenCookie } from '../support';
+
+const userFlowProvider: UserFlowProvider = {
+  flowOptions: { name: 'Open new release' },
+  interactions: async ({ page, flow, collectOptions }) => {
+    await page.setCookie(await createSpotifyTokenCookie())
+
+    await flow.navigate(collectOptions.url, {stepName: 'Navigate to Home'});
+
+    await page.waitForSelector('h1');
+
+    await flow.startTimespan({ stepName: 'Select first new release' });
+
+    await page.click('section:nth-of-type(2) app-clickable-card:first-of-type');
+    await page.waitForSelector('main app-hero-header h1');
+
+    await flow.endTimespan();
+
+    await flow.snapshot({ stepName: 'Album page' });
+  }
+};
+
+module.exports = userFlowProvider;
+
+
