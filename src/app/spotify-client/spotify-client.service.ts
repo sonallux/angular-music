56b4b830f5618b7dfce24a2058ed99aaf0df6a63
Jonas <sonallux@gmx.de>
2023-10-01 20:36:43 +0200

    fix(login): create redirect url based on current origin (#10)
    * fix(login): create redirect url based on current origin
    
    * fix(deploy): adjust GITHUB_TOKEN permission
    
    * fix(deploy): adjust GITHUB_TOKEN permission

--- src/app/spotify-client/spotify-client.service.ts
+++ src/app/spotify-client/spotify-client.service.ts
@@ -1,9 +1,9 @@
 import { Inject, Injectable, InjectionToken } from '@angular/core';
 import { SpotifyAuthentication } from './spotify-authentication';
 import { ICacheStore } from '@spotify/web-api-ts-sdk';
+import { DOCUMENT } from '@angular/common';
 
 const CLIENT_ID = 'eda234756aae490988e32cb92412225d';
-const REDIRECT_URI = 'http://localhost:4200/callback';
 
 export const CACHE_STORE_TOKEN = new InjectionToken<ICacheStore>("spotifyCacheStoreToken")
 
@@ -14,8 +14,12 @@ export class SpotifyClientService {
 
   private readonly spotifyAuthentication: SpotifyAuthentication;
 
-  constructor(@Inject(CACHE_STORE_TOKEN) cacheStore: ICacheStore) {
-    this.spotifyAuthentication = new SpotifyAuthentication(CLIENT_ID, REDIRECT_URI, ['user-top-read'], cacheStore);
+  constructor(
+    @Inject(CACHE_STORE_TOKEN) cacheStore: ICacheStore,
+    @Inject(DOCUMENT) document: Document
+  ) {
+    const redirectUri = `${document.location.origin}/callback`;
+    this.spotifyAuthentication = new SpotifyAuthentication(CLIENT_ID, redirectUri, ['user-top-read'], cacheStore);
   }
 
   public async getAccessToken() {
