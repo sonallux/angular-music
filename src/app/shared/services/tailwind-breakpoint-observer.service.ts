43c990da1af7ce2067f352428e8f44c2625ee62a
sonallux <sonallux@gmx.de>
2023-10-25 21:24:21 +0200

    perf: add shareReplay to TailwindBreakpointObserver

--- src/app/shared/services/tailwind-breakpoint-observer.service.ts
+++ src/app/shared/services/tailwind-breakpoint-observer.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { map, Observable } from 'rxjs';
+import { map, Observable, shareReplay } from 'rxjs';
 
 // TailwindCSS breakpoints
 // see https://tailwindcss.com/docs/responsive-design
@@ -8,7 +8,7 @@ const BREAKPOINT_SM = '(min-width: 640px)';
 const BREAKPOINT_MD = '(min-width: 768px)';
 const BREAKPOINT_LG = '(min-width: 1024px)';
 const BREAKPOINT_XL = '(min-width: 1280px)';
-const BREAKPOINT_2XL= '(min-width: 1536px)';
+const BREAKPOINT_2XL = '(min-width: 1536px)';
 
 export enum Breakpoint {
   XS = 0,
@@ -41,8 +41,8 @@ export class TailwindBreakpointObserver {
           return Breakpoint.SM;
         }
         return Breakpoint.XS;
-      })
+      }),
+      shareReplay({bufferSize: 1, refCount: true})
     );
   }
-
 }
