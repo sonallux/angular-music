e7c5b191d4c154ae14f15da8e3f614ad05ea584a
sonallux <sonallux@gmx.de>
2024-01-02 19:02:17 +0100

    feat: refactor search-box

--- src/app/main-layout/search-box/search-box.component.spec.ts
+++ src/app/main-layout/search-box/search-box.component.spec.ts
@@ -1,9 +1,10 @@
 import { SearchBoxComponent } from './search-box.component';
 import { render, screen } from '@testing-library/angular';
+import { MainLayoutModule } from '../main-layout.module';
 
 describe('SearchBoxComponent', () => {
   it('should render', async () => {
-    await render(SearchBoxComponent);
+    await render(SearchBoxComponent, { imports: [MainLayoutModule] });
 
     expect(await screen.findByRole('searchbox')).toBeDefined();
   });
