15fb08d54935c2cbcc57ca2e947a28e526f4136a
sonallux <sonallux@gmx.de>
2024-01-17 21:33:26 +0100

    chore(lighthouse): use default desktop throttling

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,5 +1,5 @@
 const desktopSettings = {
-  formFactor: "desktop",
+  formFactor: 'desktop',
   // Copied from https://github.com/GoogleChrome/lighthouse/blob/dd8feb782652a962451aab6ac255c36e897c5afd/core/config/constants.js#L69
   screenEmulation: {
     mobile: false,
@@ -10,21 +10,26 @@ const desktopSettings = {
   },
   // Copied from https://github.com/GoogleChrome/lighthouse/blob/dd8feb782652a962451aab6ac255c36e897c5afd/core/config/constants.js#L84
   emulatedUserAgent:
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+  // Copied from https://github.com/GoogleChrome/lighthouse/blob/dd8feb782652a962451aab6ac255c36e897c5afd/core/config/constants.js#L42-L49
+  throttling: {
+    rttMs: 40,
+    throughputKbps: 10 * 1024,
+    cpuSlowdownMultiplier: 1,
+    requestLatencyMs: 0, // 0 means unset
+    downloadThroughputKbps: 0,
+    uploadThroughputKbps: 0,
+  },
 };
 
 module.exports = {
   ci: {
     collect: {
       numberOfRuns: 5,
-      puppeteerScript: "./scripts/puppetter-script.js",
+      puppeteerScript: './scripts/puppeteer-script.js',
       settings: {
-        onlyCategories: ["performance"],
-        ...(process.env["LIGHTHOUSE_DESKTOP"] ? desktopSettings : {}),
-
-        // Always use the default mobileSlow4G as this influences the performance score heavily
-        // https://github.com/GoogleChrome/lighthouse/blob/dd8feb782652a962451aab6ac255c36e897c5afd/core/config/constants.js#L21C3-L21C15
-        // throttling: constants.throttling.desktopDense4G,
+        onlyCategories: ['performance'],
+        ...(process.env['LIGHTHOUSE_DESKTOP'] ? desktopSettings : {}),
       },
     },
   },
