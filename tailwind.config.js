1081c0780eaf4ada1cd60c6290400187cd3ea509
sonallux <sonallux@gmx.de>
2024-06-02 10:37:37 +0200

    feat(card-list): replace resize observer with CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,36 @@
-const plugin = require("tailwindcss/plugin");
+const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  safelist: [{ pattern: /grid-cols-[0-9]+/ }],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       colors: {
-        "spotify-green": {
-          50: "#e6f7ae",
-          100: "#c3eacb",
-          200: "#9cdcaa",
-          300: "#71cf88",
-          400: "#4cc46e",
-          500: "#1db954",
-          600: "#10aa4a",
-          700: "#00973e",
-          800: "#006833",
-          900: "#00671e",
+        'spotify-green': {
+          50: '#e6f7ae',
+          100: '#c3eacb',
+          200: '#9cdcaa',
+          300: '#71cf88',
+          400: '#4cc46e',
+          500: '#1db954',
+          600: '#10aa4a',
+          700: '#00973e',
+          800: '#006833',
+          900: '#00671e',
         },
 
         // overwrite the default gray color palette with the $grey-palette from @angular/material
         gray: {
-          50: "#fafafa",
-          100: "#f5f5f5",
-          200: "#eeeeee",
-          300: "#e0e0e0",
-          400: "#bdbdbd",
-          500: "#9e9e9e",
-          600: "#757575",
-          700: "#616161",
-          800: "#424242",
-          900: "#212121",
+          50: '#fafafa',
+          100: '#f5f5f5',
+          200: '#eeeeee',
+          300: '#e0e0e0',
+          400: '#bdbdbd',
+          500: '#9e9e9e',
+          600: '#757575',
+          700: '#616161',
+          800: '#424242',
+          900: '#212121',
         },
       },
     },
@@ -56,12 +55,12 @@ module.exports = {
   plugins: [
     // https://github.com/tailwindlabs/tailwindcss/discussions/10190
     plugin(({ addVariant }) =>
-      addVariant("search-cancel-button", "&::-webkit-search-cancel-button"),
+      addVariant('search-cancel-button', '&::-webkit-search-cancel-button'),
     ),
     plugin(({ matchUtilities, theme }) =>
       matchUtilities(
-        { "filter-opacity": (value) => ({ filter: `opacity(${value})` }) },
-        { values: theme("filterOpacity") },
+        { 'filter-opacity': (value) => ({ filter: `opacity(${value})` }) },
+        { values: theme('filterOpacity') },
       ),
     ),
   ],
