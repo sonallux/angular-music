38f8d92eea23901d24307acd3b04954956911582
sonallux <sonallux@gmx.de>
2024-11-04 07:53:52 +0100

    chore(deps): update to angular material 18

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { SpotifyClientService } from './spotify-client/spotify-client.service';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [AppComponent, MainLayoutComponent, RouterTestingModule],
       providers: [
         { provide: SpotifyClientService, useValue: { isAuthenticated: () => false } },
         provideHttpClient(withInterceptorsFromDi()),
